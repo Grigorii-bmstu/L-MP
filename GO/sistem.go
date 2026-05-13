e87fde7d769ef0c36c7e445267af4bbed703582f
Gr <Gr>
2026-05-13 21:51:21 +0300

    fix

--- GO/sistem.go
+++ GO/sistem.go
@@ -15,6 +15,7 @@ func (f1 Fraction) MP(f2 Fraction) Fraction {
 		d: f1.d * f2.d,
 	})
 } //Умножение
+
 func MPMany(fractions ...Fraction) Fraction {
 	res := Fraction{}
 	res.n, res.d = 1, 1
@@ -26,6 +27,7 @@ func MPMany(fractions ...Fraction) Fraction {
 	}
 	return res
 } //Умножение многих
+
 // region Вспомогательные функции (abs / gcd)
 func abs(x int) int {
 	if x < 0 {
@@ -42,7 +44,11 @@ func gcd(a, b int) int {
 }
 
 // endregion
+
 func Normalize(fraction Fraction) Fraction {
+	if fraction.d == 0 {
+		return fraction
+	}
 	if fraction.d < 0 {
 		fraction.d = -fraction.d
 		fraction.n = -fraction.n
@@ -52,6 +58,7 @@ func Normalize(fraction Fraction) Fraction {
 	fraction.d /= c
 	return fraction
 } //Нормализация
+
 func (f1 Fraction) Div(f2 Fraction) Fraction {
 	res := Fraction{
 		n: f1.n * f2.d,
@@ -59,6 +66,7 @@ func (f1 Fraction) Div(f2 Fraction) Fraction {
 	}
 	return Normalize(res)
 } //Деление
+
 func (f1 Fraction) Sub(f2 Fraction) Fraction {
 	res := Fraction{
 		n: f1.n*f2.d - f1.d*f2.n,
@@ -66,40 +74,77 @@ func (f1 Fraction) Sub(f2 Fraction) Fraction {
 	}
 	return Normalize(res)
 } //Вычитание
-n :=
-matrix := make([][]Fraction, n)
-for i := 0; i < n; i++ {
-matrix[i] = make([]Fraction, n+1)
-}
-func Gaus(matrix [][]Fraction) Fraction {
-	for i := 0; i < len(matrix); i++ {
-		Pivot := Fraction{
-			n: matrix[i][i].n,
-			d: matrix[i][i].d,
-		}
 
-		if Pivot.n == 0 {//если опорный 0 то в цикле ищем как его опустить или получаем ошибку
-			for j := i+1; j < len(matrix); j++ {
+func (f1 Fraction) Sum(f2 Fraction) Fraction {
+	res := Fraction{
+		n: f1.n*f2.d + f1.d*f2.n,
+		d: f1.d * f2.d,
+	}
+	return Normalize(res)
+} //Сложение
+
+func Gaus(matrix [][]Fraction) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		Pivot := matrix[i][i]
+
+		if Pivot.n == 0 { //если опорный 0 то в цикле ищем как его опустить или получаем ошибку
+			found := false
+			for j := i + 1; j < n; j++ {
 				if matrix[j][i].n != 0 {
 					matrix[i], matrix[j] = matrix[j], matrix[i]
-				} else if matrix[i][j].n == 0 && matrix[i][len(matrix[i])-1].n == 0 {
-					fmt.Print('No solution')
-				} else {
-					fmt.Print('infinity')
+					Pivot = matrix[i][i]
+					found = true
+					break
 				}
 			}
-		}//теперь можем начинать вычисления по идее
-		for q:=i+1; q < len(matrix); q++ {
-			var K Fraction = Pivot.Div(matrix[q][i])
-			K = Normalize(K)
-			for r:=i; r < len(matrix[q]); r++ {
-				matrix[q][r] = (K.MP(matrix[q][r])).Sub(matrix[i][r])
-				matrix[q][r] = Normalize(matrix[q][r])
+			if !found {
+				fmt.Print("No solution")
+				return
+			}
+		} //теперь можем начинать вычисления по идее
 
+		for q := i + 1; q < n; q++ {
+			var K Fraction = matrix[q][i].Div(Pivot)
+			for r := i; r <= n; r++ {
+				matrix[q][r] = matrix[q][r].Sub(K.MP(matrix[i][r]))
+				//привели матрицу в треугольный вид
 			}
 		}
-
-
-
 	}
+
+	x := make([]Fraction, n)
+	for i := n - 1; i >= 0; i-- {
+		if matrix[i][i].n == 0 {
+			fmt.Print("No solution")
+			return
+		}
+		res := matrix[i][n]
+		//будем считать сумму известных элементов
+		for j := i + 1; j < n; j++ {
+			res = res.Sub(matrix[i][j].MP(x[j]))
+		}
+		x[i] = res.Div(matrix[i][i])
+	}
+
+	for _, val := range x {
+		fmt.Printf("%d/%d\n", val.n, val.d)
+	}
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	matrix := make([][]Fraction, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]Fraction, n+1)
+		for j := 0; j <= n; j++ {
+			var val int
+			fmt.Scan(&val)
+			matrix[i][j] = Fraction{val, 1}
+		}
+	}
+
+	Gaus(matrix)
 }
