41abb1e51d2cad279534ac37bb33a7e1567eeaea
Gr <Gr>
2026-05-16 13:41:10 +0300

    debug fixes

--- GO/polish.go
+++ GO/polish.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strconv"
 	"strings"
 	"text/scanner"
 )
 
 func evel(i *int, tokens []string) int {
-	if tokens[*i] == ")" {
-		*i++
-	}
+
 	if tokens[*i] == "(" {
 		*i++
 		v := tokens[*i]
@@ -29,13 +30,14 @@ func evel(i *int, tokens []string) int {
 		*i++
 		return k
 	}
+	return 0
 }
 
 func Polish(src string) int {
 	//region Scanner
 	var s scanner.Scanner
 	s.Init(strings.NewReader(src))
-
+	s.Mode = scanner.ScanIdents | scanner.ScanStrings | scanner.ScanComments
 	var tokens []string
 	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
 		tokens = append(tokens, s.TokenText())
@@ -44,4 +46,19 @@ func Polish(src string) int {
 	idx := 0
 	return evel(&idx, tokens)
 
+}
+func main() {
+
+	sc := bufio.NewScanner(os.Stdin)
+	var fullInput strings.Builder
+
+	for sc.Scan() {
+		fullInput.WriteString(sc.Text())
+		fullInput.WriteString(" ")
+	}
+
+	expr := strings.TrimSpace(fullInput.String())
+	if expr != "" {
+		fmt.Println(Polish(expr))
+	}
 }
